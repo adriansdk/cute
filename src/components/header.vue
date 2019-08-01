185585bedc56548a691f81f1717afb910563ca00
Adrian <adrianivsdk@gmail.com>
2019-08-01 10:52:23 -0400

    Finished setting up the vue-router and added a responsive navbar.

---
+++ src/components/header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="header">
+    <h1>whatzup GSAGAW</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "header",
+  props: {
+    msg: String
+  }
+};
+
+</script>
+
+<style scoped>
+
+h1{
+  color:black;
+}
+
+</style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="myNav">
+    <nav class="navbar navbar-expand-lg sticky-top">
+      <router-link to="/" class="navbar-brand">CUTE</router-link>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarLinks" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarLinks">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item start">
+            <router-link to="/aww" class="nav-link">aww</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/faith-restored" class="nav-link">faith restored</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/me-irl" class="nav-link">me irl</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/login" class="nav-link">login</router-link>
+          </li>
+        </ul>  
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "navbar",
+  props: {
+    msg: String
+  },
+}
+
+</script>
+
+<style scoped>
+
+.navbar{
+  background-color:#EE76A5;
+  width: 100%;
+}
+
+.navbar .navbar-brand, .nav-link{
+  margin: auto 0px auto 15px;
+}
+
+.navbar a{
+  color:white;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  font-weight: bold;
+  font-size: 30px;
+  white-space: nowrap;
+}
+
+.navbar a:hover{
+  color:aqua;
+  text-shadow: black 2px 2px;
+}
+
+.navbar-toggler-icon{
+  background-color: white;
+}
+
+</style>
